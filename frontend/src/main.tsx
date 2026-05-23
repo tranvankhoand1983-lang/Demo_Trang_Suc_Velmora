96740e1c4132abb1b13e25849b4b37506a687915
tranvankhoand1983-lang <tranvankhoand1983@gmail.com>
2026-05-24 01:53:48 +0700

    feat: wrap application with GoogleOAuthProvider in main entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const GOOGLE_CLIENT_ID = "737744350806-15p9aemqhfdmscfssf70pibtn1n51ved.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "810610204634-3otf016226e78vtaj1gqr6gfu76peur9.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
